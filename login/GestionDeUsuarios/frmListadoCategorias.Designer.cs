872750bd4ab481f3154a829374157a7003ca47f1
Kevin Caicedo <kevincaicedoalvarez@gmail.com>
2026-08-29 02:04:22 -0500

    a

---
+++ login/GestionDeUsuarios/frmListadoCategorias.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace login.GestionDeUsuarios
+{
+    partial class frmListadoCategorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rvwCategorias = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // rvwCategorias
+            // 
+            this.rvwCategorias.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rvwCategorias.Location = new System.Drawing.Point(0, 0);
+            this.rvwCategorias.Name = "rvwCategorias";
+            this.rvwCategorias.ServerReport.BearerToken = null;
+            this.rvwCategorias.Size = new System.Drawing.Size(800, 450);
+            this.rvwCategorias.TabIndex = 0;
+            // 
+            // frmListadoCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rvwCategorias);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmListadoCategorias";
+            this.Text = "frmListadoCategorias";
+            this.Load += new System.EventHandler(this.frmListadoCategorias_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rvwCategorias;
+    }
+}
---
+++ login/GestionDeUsuarios/frmListadoCategorias.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login.GestionDeUsuarios
+{
+    public partial class frmListadoCategorias : Form
+    {
+        public frmListadoCategorias()
+        {
+            InitializeComponent();
+        }
+
+        private void frmListadoCategorias_Load(object sender, EventArgs e)
+        {
+
+            this.rvwCategorias.RefreshReport();
+        }
+    }
+}
