74bbefa10f313bbacffafd61cc66b4491acbfbae
Kevin Caicedo <kevincaicedoalvarez@gmail.com>
2026-09-18 00:10:12 -0500

    eeee

--- login/Promciones/Form1.cs
+++ login/Promciones/Form1.cs
@@ -10,7 +10,7 @@ namespace login.Promciones
 {
     public partial class frmCrearPromocion : Form
     {
-        string conexionString = @"Server=string conexionString = csConectaSQL.CadenaConexion;;Database=ComplejoDeportivo;Integrated Security=True;TrustServerCertificate=True;";
+        string conexionString = csConectaSQL.CadenaConexion;
         private int promocionID = 0;
         private bool modoEdicion = false;
 
